ff8315f03c82d0dc2b0995ce87662b6a93c3e69e
Miras <mirask@qaimaq.com>
2018-02-21 12:21:16 +0600

    added about and contacts pages(styles and scripts not working)

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,13 +15,6 @@ namespace KCell_Solutions.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
@@ -32,6 +25,20 @@ namespace KCell_Solutions.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+        public IActionResult Contacts()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
         }
     }
 }

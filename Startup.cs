8a4049c5ff1816f0891b8de325447bff75e1cb2a
Miras <mirask@qaimaq.com>
2018-03-29 14:24:47 +0600

    structure for KCell Solutions

--- Startup.cs
+++ Startup.cs
@@ -87,7 +87,7 @@ namespace KCell_Solutions
 
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Home}/{action=Index}");
             });
         }
     }
